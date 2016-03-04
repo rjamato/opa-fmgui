07f62bcfc8faf0836e5ab90b52777bde5612c62c
fisherma <margarita.fisher@intel.com>
2016-03-04 14:48:26 -0500

    Removed help jar files
    Replaced help jar files and added jar file creation to build.xml file

---
+++ help/consolehelp/GUID-F80D11C1-C5DF-4967-A7BB-F46A2828FEC9.hs
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN" "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+<helpset version="1.0">
+   <title>Intel® Omni-Path Fabric Suite FastFabric CLI </title>
+   <maps>
+      <homeID>GUID-5DDF2A89-BDEC-4489-AA82-1E2F90D9CF2B</homeID>
+      <mapref location="map.xml"/>
+   </maps>
+   <view>
+      <name>TOC</name>
+      <label>TOC</label>
+      <type>javax.help.TOCView</type>
+      <data>toc.xml</data>
+   </view>
+   <view mergetype="javax.help.AppendMerge">
+      <name>index</name>
+      <label>Index</label>
+      <type>javax.help.IndexView</type>
+      <data>index.xml</data>
+   </view>
+   <view>
+      <name>Search</name>
+      <label>Search</label>
+      <type>javax.help.SearchView</type>
+      <data engine="com.sun.java.help.search.DefaultSearchEngine"> JavaHelpSearch </data>
+   </view>
+   <presentation default="true">
+      <image>logo</image>
+      <title>Intel® Omni-Path Fabric Suite FastFabric CLI </title>
+   </presentation>
+</helpset>
---
+++ help/onlinehelp/GUID-02097852-C3C6-4C04-B7B5-3DADCB0EFF62.hs
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN" "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+<helpset version="1.0">
+   <title>Fabric Manager GUI Help </title>
+   <maps>
+      <homeID>logo</homeID>
+      <mapref location="map.jhm"/>
+   </maps>
+   <view>
+      <name>TOC</name>
+      <label>TOC</label>
+      <type>javax.help.TOCView</type>
+      <data>toc.xml</data>
+   </view>
+   <view mergetype="javax.help.AppendMerge">
+      <name>index</name>
+      <label>Index</label>
+      <type>javax.help.IndexView</type>
+      <data>index.xml</data>
+   </view>
+   <view>
+      <name>Search</name>
+      <label>Search</label>
+      <type>javax.help.SearchView</type>
+      <data engine="com.sun.java.help.search.DefaultSearchEngine"> JavaHelpSearch </data>
+   </view>
+   <presentation default="true">
+      <image>logo</image>
+      <title>Fabric Manager GUI Help </title>
+   </presentation>
+</helpset>
